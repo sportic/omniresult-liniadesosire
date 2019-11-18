98f7a9ef3fb1b2efb4f98adba78a1628b3c7b566
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-11-18 12:16:31 +0200

    update gen

--- src/Parsers/ResultsPage.php
+++ src/Parsers/ResultsPage.php
@@ -74,7 +74,7 @@ class ResultsPage extends AbstractParser
 
         $parameters['posCategory'] = $config['raceStanding'];
         $parameters['posGender'] = $config['raceCategoryStanding'];
-        $parameters['posGen'] = $config['raceCategoryStanding'];
+        
         $parameters['time'] = Helper::durationToSeconds($config['duration']);
         $parameters['bib'] = $config['raceNumber'];
         $parameters['id'] = $config['raceParticipantID'];
